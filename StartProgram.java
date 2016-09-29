91a021d37e04eaba77aa28b71057a99551839a92
Ormbunkelund <stinafernlund@gmail.com>
2016-09-29 14:43:50 +0200

    Update StartProgram.java
    adding fab text

--- StartProgram.java
+++ StartProgram.java
@@ -3,6 +3,7 @@ package grupptre;
 public class StartProgram {
 
 	public static void main(String[] args) {
+		System.out.println("starting this fab program again");
 		// Method to start program.
 
 	}
